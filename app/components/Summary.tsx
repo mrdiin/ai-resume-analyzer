74a8421e617ca214bb897afc7ed3de39c2fd21f1
Botshelo Brandon Tidimalo <botshelo@innovateium.co.bw>
2025-08-10 23:04:55 +0200

    feat: resume feedback components + accordion

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,5 +1,41 @@
+import ScoreBadge from './ScoreBadge';
+import ScoreGuage from './ScoreGuage';
+
+const Catergory = ({ title, score }: { title: string; score: number }) => {
+  const textColor = score > 70 ? 'text-green-600' : score > 49 ? 'text-yellow-600' : 'text-red-600';
+
+  return (
+    <div className="resume-summary">
+      <div className="category">
+        <div className="flex flex-row gap-2 items-center justify-center">
+          <p className="text-2xl">{title}</p>
+          <ScoreBadge score={score} />
+        </div>
+        <p className="text-2xl">
+          <span className={textColor}>{score}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
 const Summary = ({ feedback }: { feedback: Feedback }) => {
-  return <div>Summary</div>;
+  return (
+    <div className="bg-white rounded-2xl shadow-md w-full">
+      <div className="flex flex-row items-center p-4 gap-8">
+        <ScoreGuage score={feedback.overallScore} />
+
+        <div className="flex flex-col gap-2">
+          <h2 className="text-black text-2xl font-bold">Your Resume Score</h2>
+          <p className="text-sm text-gray-500">This score is calculated based on the variables listed below</p>
+        </div>
+      </div>
+      <Catergory title="Tone & Style" score={feedback.toneAndStyle.score} />
+      <Catergory title="Content" score={feedback.content.score} />
+      <Catergory title="Structure" score={feedback.structure.score} />
+      <Catergory title="Skills" score={feedback.skills.score} />
+    </div>
+  );
 };
 
 export default Summary;
